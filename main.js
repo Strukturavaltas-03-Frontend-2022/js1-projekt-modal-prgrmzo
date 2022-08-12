6c2b146975a2db363219bdce2268f8b13880df12
Zo <pahyzoard@outlook.hu>
2022-08-12 16:33:40 +0200

    Függvények kiszervezése, tabindexek.

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
-const myModal = document.querySelector('#myModal');
-const modalInvoke = document.querySelector('.modal-btn');
-const modalClose = document.querySelector('.close');
-const modalOkey = document.querySelector('.btn-okey');
-const modalCancel = document.querySelector('.btn-cancel');
-const body = document.querySelector('body');
+const myModal = document.querySelector("#myModal");
+const modalInvoke = document.querySelector(".modal-btn");
+const modalClose = document.querySelector(".close");
+const modalOkey = document.querySelector(".btn-okey");
+const modalCancel = document.querySelector(".btn-cancel");
+const body = document.querySelector("body");
 
-modalInvoke.addEventListener('click', () => {
-    myModal.classList.remove('modal-close');
-    myModal.classList.add('modal-open');
-    body.classList.remove('active');
-})
+modalInvoke.addEventListener("click", () => {
+  myModal.classList.remove("modal-close");
+  myModal.classList.add("modal-open");
+  body.classList.remove("active");
+});
 
-modalClose.addEventListener('click', () => {
-    myModal.classList.add('modal-close');
-    myModal.classList.remove('modal-open');
-    body.classList.add('active');
-})
+modalClose.addEventListener("click", classListHack);
 
-window.onclick = function(event) {
-    if (event.target == myModal) {
-        myModal.classList.add('modal-close');
-        myModal.classList.remove('modal-open');
-        body.classList.add('active');
-    }
+window.onclick = function (event) {
+  if (event.target == myModal) {
+    classListHack();
   }
+};
 
-modalOkey.addEventListener('click', () => {
-    myModal.classList.add('modal-close');
-    myModal.classList.remove('modal-open');
-    body.classList.add('active');
-})
+modalOkey.addEventListener("click", classListHack);
 
-modalCancel.addEventListener('click', () => {
-    myModal.classList.add('modal-close');
-    myModal.classList.remove('modal-open');
-    body.classList.add('active');
-})
+modalCancel.addEventListener("click", classListHack);
+
+function classListHack() {
+  myModal.classList.add("modal-close");
+  myModal.classList.remove("modal-open");
+  body.classList.add("active");
+}
